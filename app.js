e848e785b454b8249fa48ffe015ea38d83c4e953
Prasad2854 <122988547+Prasad2854@users.noreply.github.com>
2023-12-01 19:45:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
 const express = require("express");
 const mysql = require("mysql2");
-dotenv.config();
+
 const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// const dbHost = process.env.DB_HOST;
+// const dbUser = process.env.DB_USER;
+// const dbPassword = process.env.DB_PASSWORD;
+// const dbName = process.env.DB_NAME;
+
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
 // Set up body parser to handle form data
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // MySQL database connection
-const pool = mysql.createPool({
+const pool1 = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "Prasad@2854",
@@ -20,7 +31,7 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
 });
-
+const pool = mysql.createPool(dbConfig);
 // Route to render the HTML form
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -37,7 +48,7 @@ app.post("/process", (req, res) => {
   const aadharNumber = req.body.aadharNumber;
 
   // MySQL query to insert data
-  pool.query(
+  pool1.query(
     "INSERT INTO registration (name,middleName, lastname, dob,gender, phoneNumber,aadharNumber) VALUES (?, ?, ?, ?, ?, ?,?)",
     [firstName, middleName, lastName, dob, gender, phoneNumber, aadharNumber],
     (error, results) => {
