394f1172b02d434d147a0c26bd1efd8245b78c30
Prasad2854 <122988547+Prasad2854@users.noreply.github.com>
2023-12-04 07:46:32 +0530

    Update5 app.js

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const mysql = require("mysql2");
-const https = require("https");
+
 const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// const dbHost = process.env.DB_HOST;
+// const dbUser = process.env.DB_USER;
+// const dbPassword = process.env.DB_PASSWORD;
+// const dbName = process.env.DB_NAME;
 
 const dbConfig = {
   host: process.env.DB_HOST,
@@ -38,23 +43,36 @@ app.post("/process", (req, res) => {
   const middleName = req.body.middleName;
   const dob = req.body.dob;
   const phoneNumber = req.body.phoneNumber;
-
-  const gender = req.body.gender || "Not specified";
   const aadharNumber = req.body.aadharNumber;
+  const gender = req.body.gender || "Not specified";
+  const licenceNumber = req.body.licenceNumber;
+  const panNumber = req.body.panNumber;
+  const email = req.body.email;
 
   // MySQL query to insert data
   pool1.query(
-    "INSERT INTO registration (firstName,middleName, lastname, dob,gender, phoneNumber,aadharNumber) VALUES (?, ?, ?, ?, ?, ?,?)",
-    [firstName, middleName, lastName, dob, gender, phoneNumber, aadharNumber],
+    "INSERT INTO registration (firstName,middleName, lastname, dob,gender, phoneNumber,aadharNumber,licenceNumber,panNumber, email) VALUES (?, ?, ?, ?, ?, ?,?, ?, ?, ?)",
+    [
+      firstName,
+      middleName,
+      lastName,
+      dob,
+      gender,
+      phoneNumber,
+      aadharNumber,
+      licenceNumber,
+      panNumber,
+      email,
+    ],
     (error, results) => {
       if (error) throw error;
       res.send(`Your added details are \n ${firstName}`);
     }
   );
 });
-const httpsSever = https.createServer(app);
+
 // Start the server
 // const port = 3000;
-httpsSever.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
